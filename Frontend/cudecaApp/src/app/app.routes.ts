e3632dddccaea6792b39c8845e1fb3998124f4a8
jesusggar <jegagaitan@gmail.com>
2025-12-02 18:23:22 +0100

    feat: guard for sign-up

--- Frontend/cudecaApp/src/app/app.routes.ts
+++ Frontend/cudecaApp/src/app/app.routes.ts
@@ -7,12 +7,12 @@ import { EmptyLayoutComponent } from './layouts/empty-layout/empty-layout.compon
 import { DonationComponent } from './donation/donation.component';  // Importa tu DonationComponent
 import { EventosComponent } from './eventos/eventos.component';  // Importa el EventosComponent
 import { publicGuard } from './guards/public.guard';
-
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Ruta por defecto que carga el HomeComponent
   { path: 'log-in', component: LoginComponent, canActivate: [publicGuard]}, // Ruta para login
-  { path: 'sign-up', component: SignUpComponent},  // Ruta para registrarse
+  { path: 'sign-up', component: SignUpComponent, canActivate: [authGuard]},  // Ruta para registrarse
   { path: 'donation', component: DonationComponent }, // Ruta para donaciones
   { path: 'eventos', component: EventosComponent }, // Ruta para eventos
 ];
